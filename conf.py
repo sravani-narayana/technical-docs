5f0ed565bd8221d0d74ecf43ea1fcaff9d21f7a3
sravani-narayana <79193240+sravani-narayana@users.noreply.github.com>
2021-02-18 16:50:26 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Github Documentation'
+copyright = '2020, Sravani'
+author = 'Sravani Narayana'
 
 # The full version, including alpha/beta/rc tags
 release = ''
